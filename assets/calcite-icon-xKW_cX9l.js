0cbeb93c99bfd14f0843cd02d8903c650fe53d49
Nikita Friesen <nikitafriesen74@gmail.com>
2024-07-12 23:22:14 +0200

    Updates

---
+++ assets/calcite-icon-xKW_cX9l.js
@@ -0,0 +1,5 @@
+import{I as o,d as t}from"./icon-C8ylUFO_.js";import"./index-Cev3ONi_.js";import"./dom-CHkDf_0S.js";import"./guid-6Y9utpk5.js";import"./observers-BFaokgyp.js";/*!
+ * All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+ * See https://github.com/Esri/calcite-design-system/blob/dev/LICENSE.md for details.
+ * v2.10.1
+ */const p=o,s=t;export{p as CalciteIcon,s as defineCustomElement};
---
+++ assets/calcite-loader-DMEwlVRD.js
@@ -0,0 +1,5 @@
+import{L as e,d as o}from"./loader-CWQQ_exK.js";import"./index-Cev3ONi_.js";import"./guid-6Y9utpk5.js";/*!
+ * All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+ * See https://github.com/Esri/calcite-design-system/blob/dev/LICENSE.md for details.
+ * v2.10.1
+ */const i=e,n=o;export{i as CalciteLoader,n as defineCustomElement};
